f1d62b3ea49d500f9b6ac08a68c80f0e70f1e59a
ViniciusMantorval <VMGMaster0@gmail.com>
2025-10-02 09:17:42 -0300

    Update cadastro.js

--- frontend/cadastro/cadastro.js
+++ frontend/cadastro/cadastro.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         senha: data.senha,
         cnpj: data.cnpj
       };
-      url = 'http://traineasy.selfip.com:3000/empresas';
+      url = '/empresas';
 
     try {
       const response = await fetch(url, {
@@ -50,3 +50,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
