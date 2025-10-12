2a86eeca6e2198291baf5e3910d4b16ae6c19bed
ViniciusMantorval <VMGMaster0@gmail.com>
2025-10-11 22:12:40 -0300

    Update pagamento.js

--- frontend/pagamento/pagamento.js
+++ frontend/pagamento/pagamento.js
@@ -638,13 +638,13 @@ function openHelp() {
 }
 
 function logout() {
-  showLoadingOverlay();
-  
-  setTimeout(() => {
-    hideLoadingOverlay();
-    showNotification('Logout realizado com sucesso!', 'success');
-    window.location.href = '../index/index.html';
-  }, 2000);
+  if (confirm('Tem certeza que deseja sair?')) {
+    showNotification('Fazendo logout...', 'info');
+    // Aqui você pode adicionar a lógica de logout
+    setTimeout(() => {
+      window.location.href = '../login/login.html';
+    }, 1000);
+  }
 }
 
 // Utilitários para formatação
@@ -672,6 +672,7 @@ function formatCPF(cpf) {
   }
   return cpf;
 }
+
 
 
 
